c707e2a6902ab54da2e7a30dfd74f0b20ae69421
Hayyan Alkhattab <hayyanalkhatab12@gmail.com>
2025-01-15 15:13:15 +0300

    2.1 kode guncelleme

--- src/arayuz/ArayuzVatandas.java
+++ src/arayuz/ArayuzVatandas.java
@@ -1,10 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package arayuz;
 
 import arayuz.vatandasFonksyonlari.*;
+import yonetim.SuYonetimi;
 
 public class ArayuzVatandas extends javax.swing.JFrame {
 
